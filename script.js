0298d98fbc06201a73c5db2cfe9cfaf47d024d8d
Ropan Barmon <68578326+ropan-barmon@users.noreply.github.com>
2020-10-07 18:50:49 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 
+ // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 40,
+        loop: true
+    });
